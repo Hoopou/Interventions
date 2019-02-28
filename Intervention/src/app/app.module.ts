50829f96745c8c23464dfc8f9189f3663aa60457
Vincent_Boutot <VincentBoutot@live.ca>
2019-02-28 14:16:47 -0500

    FIN DU TP 5

--- Intervention/src/app/app.module.ts
+++ Intervention/src/app/app.module.ts
@@ -3,14 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AngularFontAwesomeModule} from 'angular-font-awesome';
+import { BienvenueComponent } from './bienvenue/bienvenue.component';
+import { AccueilComponent } from './accueil/accueil.component';
+import { ProblemeComponent } from './probleme/probleme.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BienvenueComponent,
+    AccueilComponent,
+    ProblemeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AngularFontAwesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
