e3579a013ded98566952e5095c6578564533da51
Vincent_Boutot <VincentBoutot@live.ca>
2019-02-28 14:44:16 -0500

    avec le Routing Table

--- Intervention/src/app/app.component.ts
+++ Intervention/src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'stk-root',
+  selector: 'Inter-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
