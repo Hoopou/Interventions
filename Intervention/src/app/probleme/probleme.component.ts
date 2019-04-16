e1f8f213057ebdbae27027834d5ea9e232ac4e64
Vincent_Boutot <VincentBoutot@live.ca>
2019-04-16 09:41:18 -0400

    TP16 terminé

--- Intervention/src/app/probleme/probleme.component.ts
+++ Intervention/src/app/probleme/probleme.component.ts
@@ -27,7 +27,10 @@ export class ProblemeComponent implements OnInit {
         courrielConfirmation: [{ value: '', disabled: true }],
       }),
       telephone: [{ value: '', disabled: true }],
-      isFormulaireValide: [{value: false, disable: true}]
+      isFormulaireValide: [{ value: false, disable: true }],
+      descriptionProbleme: ['', [Validators.required, Validators.minLength(5)]],
+      noUnite: '',
+      dateProbleme: { value: Date(), disabled: true } 
     });
 
     this.categories.obtenirCategories()
